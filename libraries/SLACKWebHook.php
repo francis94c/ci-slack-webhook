3da49c39092ad3223383c906121b74fb8880c281
Francis Ilechukwu <francis94c@gmail.com>
2020-02-29 17:00:28 +0100

    Update SLACKWebHook.php

--- libraries/SLACKWebHook.php
+++ libraries/SLACKWebHook.php
@@ -20,14 +20,28 @@ class SLACKWebHook
       $this->url = $params['url'] ?? null;
     }
   }
-  
+
   /**
    * [send description]
    * @date  2020-02-29
    * @param SLACKMessage $message [description]
    */
-  public function send(SLACKMessage $message):void
+  public function send(SLACKMessage $message):bool
   {
-
+    $ch = curl_init($this->url);
+    $body = json_encode($message->toArray());
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+      'Content-Length: ' . strlen($body),
+      'Content-Type: application/json'
+    ]);
+    curl_setopt($ch, CURLOPT_USERAGENT, 'CI-SLACK-WEBHOOK-LIB');
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+    curl_exec($ch);
+    $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+    return $code == 200;
   }
 }
